476419be46bebde82b5d424e5a51226e062f2c4c
Falko Janak <contact@falkojanak.com>
2025-03-26 23:59:30 +0100

    cleanup

--- MyRestfulWebshop.API/Program.cs
+++ MyRestfulWebshop.API/Program.cs
@@ -5,10 +5,12 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-});
+builder.Services.AddControllers();
+
+//builder.Services.AddControllers().AddJsonOptions(options =>
+//{
+//    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+//});
 
 builder.Services.AddDbContext<MyRestfulWebshopContext>(options =>
     options.UseInMemoryDatabase("MyRestfulWebshopDB"));
